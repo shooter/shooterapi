60352eb00889cda68dd52356bfeafab0839148df
Wooter O'Donoghue <wooterodonoghue@gmail.com>
2009-10-20 22:08:49 +0100

    be a bit more careful with access control

--- app/controllers/my/shooters_controller.rb
+++ app/controllers/my/shooters_controller.rb
@@ -1,6 +1,12 @@
-class My::ShootersController < InheritedResources::Base
+class My::ShootersController < ApplicationController
   
   before_filter :require_user
+  
+  def index
+    @shooters = current_user.shooters.paginate(:page => params[:page])
+  end
+  
+
   
   def show
     @shooter = current_user.shooters.find_by_shortcode_url(params[:id])
@@ -14,6 +20,10 @@ class My::ShootersController < InheritedResources::Base
     redirect_to [:my, @shooter]
   end
   
+  def new
+    @shooter = current_user.shooters.new
+  end
+  
   def create
     @shooter = Shooter.new(params[:shooter])
     @shooter.user = current_user
@@ -22,6 +32,21 @@ class My::ShootersController < InheritedResources::Base
     else
       render :new
     end
+  end
+  
+  def edit
+    @shooter = current_user.shooters.find(params[:id])
+    if @shooter.update_attributes(params[:shooter])
+      redirect_to [:my, @shooter]
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @shooter = current_user.shooters.find(params[:id])
+    @shooter.destroy
+    redirect_to my_shooters_path
   end
   
 protected
